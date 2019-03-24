3b5eb5c7a6e267ddefa3a3299c5f780fb47adb6e
vic <vic96418796@gmail.com>
2019-03-24 13:49:49 +0800

    新增註冊信箱and email登入功能

---
+++ app/src/main/java/com/example/test/email_login.java
@@ -0,0 +1,113 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+
+
+
+
+
+
+public class email_login extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener authListener;
+    private String userUID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.email_login);
+        auth = FirebaseAuth.getInstance();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(
+                    @NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!=null) {
+                    Log.d("onAuthStateChanged", "登入:"+
+                            user.getUid());
+                    userUID =  user.getUid();
+                }else{
+                    Log.d("onAuthStateChanged", "已登出");
+                }
+            }
+        };
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+
+    public void login(View v){
+        final String email = ((EditText)findViewById(R.id.email))
+                .getText().toString();
+        final String password = ((EditText)findViewById(R.id.password))
+                .getText().toString();
+        Log.d("AUTH", email+"/"+password);
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("onComplete", "onComplete");
+                        if (!task.isSuccessful()){
+                            Log.d("onComplete", "登入失敗");
+                            register(email, password);
+                        }
+                    }
+                });
+    }
+
+    private void register(final String email, final String password) {
+        new AlertDialog.Builder(email_login.this)
+                .setTitle("登入問題")
+                .setMessage("無此帳號，是否要以此帳號與密碼註冊?")
+                .setPositiveButton("註冊", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        createUser(email, password);
+                    }
+                })
+                .setNeutralButton("取消", null)
+                .show();
+    }
+
+    private void createUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                String message =
+                                        task.isComplete() ? "註冊成功" : "註冊失敗";
+                                new AlertDialog.Builder(email_login.this)
+                                        .setMessage(message)
+                                        .setPositiveButton("OK", null)
+                                        .show();
+                            }
+                        });
+    }
+
+
+}
