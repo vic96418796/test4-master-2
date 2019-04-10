d64b5acef0a81ae13c70f8d03e8388413db52048
xiang <orz30344@gmail.com>
2019-04-10 22:16:22 +0800

    輸入password變＊字號

--- app/src/main/java/com/example/test/Main7Activity.java
+++ app/src/main/java/com/example/test/Main7Activity.java
@@ -3,6 +3,7 @@ package com.example.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.content.Intent;
 
@@ -13,11 +14,20 @@ public class Main7Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
         ImageButton collection = findViewById(R.id.collection);
+        Button logout = findViewById(R.id.logout);
 
         collection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (Main7Activity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent ( Main7Activity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
