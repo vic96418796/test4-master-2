c10b4a09b9df9b6d834b3c9d46bb9c001904f1a1
k <kevin821010@gmail.com>
2019-05-01 21:07:12 +0800

    左拉式選單

--- app/src/main/java/com/example/test/Main3Activity.java
+++ app/src/main/java/com/example/test/Main3Activity.java
@@ -1,23 +1,36 @@
 package com.example.test;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.support.v7.widget.Toolbar;
 
 public class Main3Activity extends AppCompatActivity {
-
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar,  R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         ImageButton account = findViewById(R.id.account);
         ImageButton food4 = findViewById(R.id.food4);
         ImageButton map = findViewById(R.id.map);
+
 
 
         food4.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +64,14 @@ public class Main3Activity extends AppCompatActivity {
         });
 
 
+    }
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.isDrawerOpen(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
 
 
     }
