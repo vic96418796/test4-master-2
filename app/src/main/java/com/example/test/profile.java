0f4f8208d1f1b341e05a5048ac50267ad14b61b8
vic <vic96418796@gmail.com>
2019-05-03 12:54:19 +0800

    Merge branch 'master' of https://github.com/vic96418796/test4-master-2
    # Conflicts:
    #	app/src/main/java/com/example/test/Main7Activity.java

--- app/src/main/java/com/example/test/profile.java
+++ app/src/main/java/com/example/test/profile.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.content.Intent;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+
 public class profile extends AppCompatActivity {
 
 
@@ -26,7 +30,7 @@ public class profile extends AppCompatActivity {
 
 
         ImageButton collection = findViewById(R.id.collection);
-        Button logout = findViewById(R.id.logout);
+        final Button logout = findViewById(R.id.logout);
         ImageButton map = findViewById(R.id.map);
 
         collection.setOnClickListener(new View.OnClickListener() {
@@ -40,8 +44,21 @@ public class profile extends AppCompatActivity {
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent ( profile.this, login_interface.class);
-                startActivity(intent);
+                new AlertDialog.Builder(profile.this)
+                        .setTitle("登出")
+                        .setMessage("確定要登出嗎?")
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (logout.callOnClick()){
+                                    Intent intent = new Intent (profile.this, email_login.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        })
+                        .setNegativeButton("取消",null)
+                        .show();
+
             }
         });
 
