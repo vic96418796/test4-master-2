21f6790ca54b59d7826df1ad0181ea0238812ed6
wtf1012goddamn <orz30344@gmail.com>
2019-05-29 22:01:22 +0800

    ccc

--- app/src/main/java/com/example/test/LabelAdapter.java
+++ app/src/main/java/com/example/test/LabelAdapter.java
@@ -1,7 +1,9 @@
 package com.example.test;
+
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,55 +21,74 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.List;
+
 public class LabelAdapter extends RecyclerView.Adapter<LabelAdapter.ViewHolder> {
-    private static final String TAG = "TEST";
+    private static final String TAG = "LabelAdapter";
     public FirebaseFirestore db;
     public List<Label> label;
     public List<Label> edit_text;
     public Context context;
     private FirebaseAuth auth;
     private String userId;
+
     public LabelAdapter(Context applicationContext, List<Label> edit_text) {
         this.edit_text = edit_text;
         this.context = context;
         this.label = label;
     }
+
+    private LabelAdapter.transPageListener mTransPageListener;//adapter跳轉fragment
+
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_label_cardview, parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_label_cardview, parent, false);
         auth = FirebaseAuth.getInstance();
         userId = auth.getCurrentUser().getUid();
         db = FirebaseFirestore.getInstance();
         return new ViewHolder(view);
     }
+
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         final String label_tags = edit_text.get(position).labelId;
         holder.label.setText(edit_text.get(position).getLabel_tags());
-        holder.mView.setOnClickListener(new View.OnClickListener() {
+
+        holder.label.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                做那個篩選的動作
-//                Context context = v.getContext();
-//                Intent intent = new Intent();
-//                intent.setClass(context,FriendInformation.class);
-//                intent.putExtra("FriendId", friend_id);
-//                Log.d(TAG,"Id: "+friend_id);
-//                context.startActivity(intent);
+                String tag = edit_text.get(position).getLabel_tags();
+                Log.d(TAG, "tag: " + tag);
+                //Send value from adapter to activity
+                Intent intent = new Intent("custom-message");
+                intent.putExtra("tag", tag);
+                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+
             }
         });
+
     }
+
     @Override
     public int getItemCount() {
         return edit_text.size();
     }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         View mView;
         public Button label;
+
         public ViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
-            label = (Button)mView.findViewById(R.id.label);
+            label = (Button) mView.findViewById(R.id.label);
         }
     }
+
+    public interface transPageListener {
+        public void onTransPageClick(String tag);
+    }//adapter跳轉fragment並攜帶需要的資料
+
+    public void setOnTransPageClickListener(transPageListener transPageListener) {
+        this.mTransPageListener = transPageListener;
+    }//adapter跳轉fragment
 }
--- app/src/main/java/com/example/test/edit_text.java
+++ app/src/main/java/com/example/test/edit_text.java
@@ -1,8 +1,12 @@
 package com.example.test;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -32,14 +36,17 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 
 public class edit_text extends AppCompatActivity {
+    private final String TAG = "edit_text";
     DatabaseReference ref;
     RecyclerView recyclerView1;
     RecyclerView recyclerView2;
@@ -56,23 +63,26 @@ public class edit_text extends AppCompatActivity {
     private ArrayList<Double> lat;
     private ArrayList<String> namelst;
     private ArrayList<String> num;
-    private ArrayList<Double>lat1;
-    private ArrayList<String>lat5;
+    private ArrayList<Double> lat1;
+    private ArrayList<String> lat5;
     private Restaurant restaurant;
     private String userId;
-    private ArrayList<String>lat11;
-    private ArrayList<Double>lat2;
-    private ArrayList<String>clat;
-    private ArrayList<Double>lat3;
-    private ArrayList<String>useridd;
-    private ArrayList<String>userid2;
-    private ArrayList<String>record;
-    private ArrayList<String>restaurant2;
-    private ArrayList<String>friendlist2;
-    private ArrayList<String>restaurantList1;
-    private ArrayList<Double>lat4;
-    ArrayList<String>friendlist1;
+    private ArrayList<String> lat11;
+    private ArrayList<Double> lat2;
+    private ArrayList<String> clat;
+    private ArrayList<Double> lat3;
+    private ArrayList<String> useridd;
+    private ArrayList<String> userid2;
+    private ArrayList<String> record;
+    private ArrayList<String> restaurant2;
+    private ArrayList<String> friendlist2;
+    private ArrayList<String> restaurantList1;
+    private ArrayList<Double> lat4;
+    ArrayList<String> friendlist1;
     private ArrayList<Double> latc;
+    LabelAdapter labelAdapter;
+    String tag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,15 +108,21 @@ public class edit_text extends AppCompatActivity {
         lat1 = new ArrayList<>();
         lat3 = new ArrayList<>();
         latc = new ArrayList<>();
+        label = new ArrayList<>();
         auth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         userId = auth.getCurrentUser().getUid();
         RestaurantList = new ArrayList<>();
-        RestaurantListAdapter = new RestaurantListAdapter(getApplicationContext(),RestaurantList);
-        recyclerView1 = (RecyclerView) findViewById(R.id.rv_hen);
+        RestaurantListAdapter = new RestaurantListAdapter(getApplicationContext(), RestaurantList);
+        recyclerView1 = findViewById(R.id.rv_zi);
         recyclerView1.setHasFixedSize(true);
         recyclerView1.setLayoutManager(new LinearLayoutManager(this));
         recyclerView1.setAdapter(RestaurantListAdapter);
+        recyclerView2 = findViewById(R.id.rv_hen);
+        recyclerView2.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView2.setLayoutManager(linearLayoutManager);
         final String currentUserID = auth.getCurrentUser().getUid();
         db.collection("Restaurant").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
@@ -128,8 +144,8 @@ public class edit_text extends AppCompatActivity {
         db.collection("Restaurant").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    for(DocumentSnapshot documentSnapshot : task.getResult()){
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot documentSnapshot : task.getResult()) {
                         restaurant = documentSnapshot.toObject(Restaurant.class);
                         String restaurant_id = documentSnapshot.getId();
                         lat.add(restaurant.getRestaurant_lat());
@@ -145,15 +161,14 @@ public class edit_text extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar,  R.string.navigation_drawer_open,
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigation_view = (NavigationView) findViewById(R.id.nav_view);
         ref = FirebaseDatabase.getInstance().getReference("resturant");
-        recyclerView1 = findViewById(R.id.rv_zi);
-        recyclerView2 = findViewById(R.id.rv_hen);
+
         searchView = findViewById(R.id.searchView);
         navigation_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -161,22 +176,22 @@ public class edit_text extends AppCompatActivity {
                 drawer.closeDrawer(GravityCompat.START);
                 int id = menuItem.getItemId();
                 if (id == R.id.nav_profile) {
-                    Intent intent = new Intent(edit_text.this,profile.class);
-                    intent.putExtra("user_id",userId);
+                    Intent intent = new Intent(edit_text.this, profile.class);
+                    intent.putExtra("user_id", userId);
                     startActivity(intent);
                     return true;
                 }
                 if (id == R.id.nav_maps) {
-                    Intent intent = new Intent(edit_text.this,MapsActivity.class);
-                    intent.putExtra("lat",lat);
-                    intent.putExtra("namelst",namelst);
-                    intent.putExtra("num",num);
-                    intent.putExtra("lat1",lat1);
-                    intent.putExtra("restaurantList1",restaurantList1);
-                    intent.putExtra("clat",clat);
-                    intent.putExtra("lat3",lat3);
-                    intent.putExtra("lat4",lat4);
-                    intent.putExtra("latc",latc);
+                    Intent intent = new Intent(edit_text.this, MapsActivity.class);
+                    intent.putExtra("lat", lat);
+                    intent.putExtra("namelst", namelst);
+                    intent.putExtra("num", num);
+                    intent.putExtra("lat1", lat1);
+                    intent.putExtra("restaurantList1", restaurantList1);
+                    intent.putExtra("clat", clat);
+                    intent.putExtra("lat3", lat3);
+                    intent.putExtra("lat4", lat4);
+                    intent.putExtra("latc", latc);
                     startActivity(intent);
                     return true;
                 }
@@ -186,72 +201,122 @@ public class edit_text extends AppCompatActivity {
                     return true;
                 }
                 if (id == R.id.nav_friend) {
-                    Intent intent = new Intent( edit_text.this,FriendList.class);
+                    Intent intent = new Intent(edit_text.this, FriendList.class);
                     startActivity(intent);
                     return true;
                 }
                 return false;
             }
         });
+        //Send value from adapter to activity
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("custom-message"));
     }
+
+    //Send value from adapter to activity
+    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            tag = intent.getStringExtra("tag");
+            Log.d(TAG,"tag: "+tag);
+            ArrayList<Restaurant> myList = new ArrayList<>();
+            for (Restaurant object : list) {
+                if (object.getRestaurant_name().toLowerCase().contains(tag.toLowerCase()) || object.getRestaurant_tags().toLowerCase().contains(tag.toLowerCase())) {
+                    myList.add(object);
+                }
+            }
+            RestaurantListAdapter adapterClass = new RestaurantListAdapter(edit_text.this, myList);
+            recyclerView1.setAdapter(adapterClass);
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        int size = label.size();
+        label.clear();
+        labelAdapter.notifyItemRangeRemoved(0,size);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-            DocumentReference docRef = db.collection("cities").document("BJ");
-            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                    resturant r = documentSnapshot.toObject(resturant.class);
-                }
-            });
-
-            db.collection("Restaurant").addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
-                    if (e != null) {
-                        // Log.d(TAG, "Error :" + e.getMessage());
-                    } else {
-                        list = new ArrayList<>();
-                        label = new ArrayList<>();
-                        for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
-                            if (doc.getType() == DocumentChange.Type.ADDED) {
-                                String restaurant_id = doc.getDocument().getId();
-                                Restaurant restaurant = doc.getDocument().toObject(Restaurant.class).withId(restaurant_id);
-                                list.add(restaurant);
+        DocumentReference docRef = db.collection("cities").document("BJ");
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                resturant r = documentSnapshot.toObject(resturant.class);
+            }
+        });
+        db.collection("Label")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Label labelObject = document.toObject(Label.class);
+                                label.add(labelObject);
                             }
+                            labelAdapter = new LabelAdapter(edit_text.this, label);
+                            recyclerView2.setAdapter(labelAdapter);
+
+                            for (int i = 0; i < label.size(); i++) {
+                                Log.d(TAG, "label" + label.get(i).getLabel_tags());
+                            }
+
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
                         }
-                        LabelAdapter LabelAdapter = new LabelAdapter(edit_text.this,label);
-                        RestaurantListAdapter adapterClass = new RestaurantListAdapter(edit_text.this,list);
-                        recyclerView1.setAdapter(adapterClass);
-                        recyclerView2.setAdapter(LabelAdapter);
-                    }
-                }
-            });
-            if (searchView != null) {
-                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextSubmit(String s) {
-                        return false;
-                    }
-                    @Override
-                    public boolean onQueryTextChange(String s) {
-                        search(s);
-                        return true;
                     }
                 });
+
+
+        db.collection("Restaurant").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if (e != null) {
+                    // Log.d(TAG, "Error :" + e.getMessage());
+                } else {
+                    list = new ArrayList<>();
+                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
+                            String restaurant_id = doc.getDocument().getId();
+                            Restaurant restaurant = doc.getDocument().toObject(Restaurant.class).withId(restaurant_id);
+                            list.add(restaurant);
+                        }
+                    }
+                    RestaurantListAdapter adapterClass = new RestaurantListAdapter(edit_text.this, list);
+                    recyclerView1.setAdapter(adapterClass);
+                }
             }
+        });
+        if (searchView != null) {
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    search(s);
+                    return true;
+                }
+            });
         }
-    private void search(String str)
-    {
-        ArrayList<Restaurant>  myList = new ArrayList<>();
-        for(Restaurant object : list)
-        {
-            if(object.getRestaurant_name().toLowerCase().contains(str.toLowerCase())||object.getRestaurant_tags().toLowerCase().contains(str.toLowerCase()))
-            {
+    }
+
+    private void search(String str) {
+        ArrayList<Restaurant> myList = new ArrayList<>();
+        for (Restaurant object : list) {
+            if (object.getRestaurant_name().toLowerCase().contains(str.toLowerCase()) || object.getRestaurant_tags().toLowerCase().contains(str.toLowerCase())) {
                 myList.add(object);
             }
         }
-        RestaurantListAdapter adapterClass = new RestaurantListAdapter(edit_text.this,myList);
+        RestaurantListAdapter adapterClass = new RestaurantListAdapter(edit_text.this, myList);
         recyclerView1.setAdapter(adapterClass);
     }
 }
